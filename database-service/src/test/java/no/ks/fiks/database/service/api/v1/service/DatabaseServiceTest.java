c28c5fe2a8494d30d3c5b820540462bbb594cd01
Axel Hjelmervik Hansen <Axel.Hjelmervik.Hansen@ks.no>
2021-08-06 15:54:39 +0200

    Lagt til tester for dropTable

--- database-service/src/test/java/no/ks/fiks/database/service/api/v1/service/DatabaseServiceTest.java
+++ database-service/src/test/java/no/ks/fiks/database/service/api/v1/service/DatabaseServiceTest.java
@@ -1,5 +1,6 @@
 package no.ks.fiks.database.service.api.v1.service;
 
+import com.sun.xml.bind.v2.TODO;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,7 +19,7 @@ class DatabaseServiceTest {
     private JdbcTemplate jdbcTemplate;
 
     private DatabaseService dbs;
-    private String tableName, resultString, inputString;
+    private String tableName, expectedResultString, inputString;
 
     @BeforeEach
     void setUp() {
@@ -40,17 +41,28 @@ class DatabaseServiceTest {
     @Test
     void testCreateTableValidStringStructure() {
         inputString = "create table TEST_S.TEST (Regionkode varchar(100))";
-        resultString = "Table created";
+        expectedResultString = "Table created";
 
-        assertEquals(resultString, dbs.createTable(jdbcTemplate, inputString));
+        assertEquals(expectedResultString, dbs.createTable(jdbcTemplate, inputString));
     }
 
     @Test
     void testCreateTableInvalidStringStructure() {
         inputString = "create table (Regionkode varchar(100)) TEST_S.TEST";
-        resultString = "Create table string didn't match the pattern, try with lower case";
+        expectedResultString = "Create table string didn't match the pattern, try with lower case";
 
-        assertEquals(resultString, dbs.createTable(jdbcTemplate, inputString));
+        assertEquals(expectedResultString, dbs.createTable(jdbcTemplate, inputString));
+    }
+
+    //TODO: Oppdater expectedResultString etter flere sjekker i DatabaseService har blitt lagt til
+    @Test
+    void testCreateTableWithAlreadyExistingTable() {
+        inputString = "create table (Regionkode varchar(100)) TEST_S.TEST";
+
+        expectedResultString = "Create table string didn't match the pattern, try with lower case";
+
+        dbs.createTable(jdbcTemplate, inputString);
+        assertEquals(expectedResultString, dbs.createTable(jdbcTemplate, inputString));
     }
 
     @Test
@@ -58,9 +70,19 @@ class DatabaseServiceTest {
         dbs.createTable(jdbcTemplate, "create table TEST_S.TEST (Regionkode int)");
 
         inputString = "drop table TEST_S.TEST";
-        resultString = "Table dropped";
+        expectedResultString = "Table dropped";
 
-        assertEquals(resultString, dbs.dropTable(jdbcTemplate, inputString));
+        assertEquals(expectedResultString, dbs.dropTable(jdbcTemplate, inputString));
+    }
+
+    @Test
+    void testDropTableInvalidStringStructure() {
+        dbs.createTable(jdbcTemplate, "create table TEST_S.TEST (Regionkode int)");
+
+        inputString = "drop TEST_S.TEST table";
+        expectedResultString = "Drop table string didn't match the pattern, try with lower case";
+
+        assertEquals(expectedResultString, dbs.dropTable(jdbcTemplate, inputString));
     }
 
     @Test
