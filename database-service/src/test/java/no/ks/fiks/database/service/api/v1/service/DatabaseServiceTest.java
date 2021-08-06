6890a875985004b4b781ee9928cf6b8508a97125
Axel Hjelmervik Hansen <Axel.Hjelmervik.Hansen@ks.no>
2021-08-06 10:44:01 +0200

    Tester, litt rydding, LF format og tab spaces
    La inn JUnit 5 dependency til root
    Satte opp skjelettet til første testklassen
    Fjernet en klasse som ikke skulle være der
    Endret pom filene til å ha 4 mellomroms indent istedenfor 2

---
+++ database-service/src/test/java/no/ks/fiks/database/service/api/v1/service/DatabaseServiceTest.java
@@ -0,0 +1,26 @@
+package no.ks.fiks.database.service.api.v1.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createTable() {
+    }
+
+    @Test
+    void dropTable() {
+    }
+}
