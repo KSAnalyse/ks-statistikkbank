a26fd472526c23c0f07100051e6f23d4d1fdc5b6
Hama Keli <hama.keli@ks.no>
2021-10-26 07:57:08 +0200

    Created Spring app

--- scheduler/src/main/java/no/ks/fiks/App.java
+++ scheduler/src/main/java/no/ks/fiks/App.java
@@ -1,7 +1,12 @@
 package no.ks.fiks;
 
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-
+        new SpringApplicationBuilder(App.class).web(WebApplicationType.NONE).run(args);
     }
 }
