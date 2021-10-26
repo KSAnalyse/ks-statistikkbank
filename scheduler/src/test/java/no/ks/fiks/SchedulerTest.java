fa7ca5fc73d0f5d059e8a3768fbe98d8225ee023
Hama Keli <hama.keli@ks.no>
2021-10-26 08:01:14 +0200

    Added some tests

--- scheduler/src/test/java/no/ks/fiks/SchedulerTest.java
+++ scheduler/src/test/java/no/ks/fiks/SchedulerTest.java
@@ -1,16 +1,58 @@
 package no.ks.fiks;
 
+import no.ks.fiks.api.v1.scheduler.Scheduler;
+import no.ks.fiks.csvreader.CsvReader;
+import no.ks.fiks.tableinfo.TableFilterAndGroups;
 import org.junit.jupiter.api.Test;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.FileNotFoundException;
+import java.util.List;
 
 class SchedulerTest {
 
     @Test
-    public void givenRunnable_whenRunIt_thenResult() throws Exception {
-        Thread thread = new Thread(new Scheduler("Scheduler executed using Thread"));
-        thread.start();
-        thread.join();
+    public void printMessage() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(30);
+        taskExecutor.setMaxPoolSize(30);
+        taskExecutor.initialize();
+        new Scheduler(taskExecutor).runApiCall();
     }
 
+    @Test
+    void splitStringIntoMap() {
+        TableFilterAndGroups test1 = new TableFilterAndGroups(
+                "SSB",
+                "12367",
+                "Økonomi",
+                "KOKregnskapsomfa0000=None",
+                "KOKregnskapsomfa0000=A"
+        );
+        TableFilterAndGroups test2 = new TableFilterAndGroups(
+                "SSB",
+                "12905",
+                "Eiendomsforvaltning",
+                "",
+                "KOKkjoenn0000!=0&ContentsCode=KOSantlarergrsk0000"
+        );
+        System.out.println(test1.getLagTabellFilter());
+        System.out.println(test2.getLagTabellFilter());
+        System.out.println(test1.getHentDataFilter());
+        System.out.println(test2.getHentDataFilter());
+    }
+
+    @Test
+    void readFromCsv() {
+        CsvReader csvReader = new CsvReader();
+        try {
+            csvReader.readFromCsv();
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        }
+        List<TableFilterAndGroups> test = csvReader.getTablesAndFilters();
+
+        test.stream().map((k) -> (k.getKildeId() + " " + k.getTabellnummer() + " " + k.getGruppenavn() + " " +
+                k.getLagTabellFilter() + " " + k.getHentDataFilter())).forEach(System.out::println);
+    }
 }
