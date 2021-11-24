a81f2cb9cbf9fcf6fa494d3adaf7b3477a5ce05b
Hama Keli <hama.keli@ks.no>
2021-11-24 15:32:12 +0100

    Added try-catch

--- database-service/src/main/java/no/ks/fiks/database/service/App.java
+++ database-service/src/main/java/no/ks/fiks/database/service/App.java
@@ -8,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public static void main( String[] args ) {
-        SpringApplication.run(App.class, args);
+        try {
+            SpringApplication.run(App.class, args);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
     }
 }
