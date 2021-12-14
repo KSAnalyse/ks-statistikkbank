f5aae978e702936b3226fed022ee3bd7c5377789
Axel Hjelmervik Hansen <Axel.Hjelmervik.Hansen@ks.no>
2021-12-14 10:12:33 +0100

    Moved the query increase from the threads to the manager

--- data-fetcher/src/main/java/no/ks/fiks/data/fetcher/api/v1/scheduler/Scheduler.java
+++ data-fetcher/src/main/java/no/ks/fiks/data/fetcher/api/v1/scheduler/Scheduler.java
@@ -128,7 +128,7 @@ public class Scheduler {
                         TimeUnit.SECONDS.sleep(10);
                     }
 
-                    if (getQueryCounter() >= 30 || getQueryCounter() + 1 > 30) {
+                    if (getQueryCounter() >= 30 || getQueryCounter() + 2 >= 30) {
                         TimeUnit.SECONDS.sleep(60);
                         resetQueryCounter();
                     }
@@ -137,16 +137,16 @@ public class Scheduler {
                     JsonNode jsonObject = mapper.readTree(getFirstJsonInQueue());
                     SsbApiCall ssbApiCall = new SsbApiCall(jsonObject.get("tableCode").asText(), 5, "131",
                             "104", "214", "231", "127");
-                    SsbApiCallTask sact = new SsbApiCallTask(dfs, ssbApiCall, getFirstJsonInQueue(), jsonObject.get("tableCode").asText(),
-                            ssbApiCall.getQuerySize(), System.nanoTime(), this);
+                    increaseQueryCounter(ssbApiCall.getQuerySize() + 2);
 
-                    if (getQueryCounter() + sact.querySize + 2 >= 30) {
+                    if (getQueryCounter() >= 30) {
                         System.out.println("[ThreadManager] Sleeping for 60s");
                         TimeUnit.SECONDS.sleep(60);
                         resetQueryCounter();
                     }
 
-                    taskExecutor.execute(sact);
+                    taskExecutor.execute(new SsbApiCallTask(dfs, ssbApiCall, getFirstJsonInQueue(), jsonObject.get("tableCode").asText(),
+                            ssbApiCall.getQuerySize(), System.nanoTime(), this));
                     threadQueries.remove(0);
                 } catch (InterruptedException ie) {
                     System.out.println("OH NO!");
@@ -162,7 +162,6 @@ public class Scheduler {
 
         private final ThreadManager monitor;
         private final SsbApiCall ssbApiCall;
-        private final int querySize;
         private final String tableCode;
         private long startTime;
         private DataFetcherService dfs;
@@ -174,7 +173,6 @@ public class Scheduler {
             this.ssbApiCall = ssbApiCall;
             this.json = json;
             this.tableCode = tableCode;
-            this.querySize = querySize;
             this.monitor = monitor;
             this.startTime = startTime;
         }
@@ -182,25 +180,10 @@ public class Scheduler {
         @Override
         public void run() {
             System.out.println("[" + tableCode + "] STARTING. " + ssbApiCall.getMetadata().getTitle() + ". Size: " + ssbApiCall.getQuerySize());
-            // Both createTable and insertData uses the SsbApiCall class which means two extra queries
-            monitor.increaseQueryCounter(querySize + 2);
             System.out.println("[" + tableCode + "] createTable: " + dfs.createTable(json));
             System.out.println("[" + tableCode + "] truncateTable: " + dfs.truncateTable(json));
             System.out.println("[" + tableCode + "] insertData: " + dfs.insertData(json));
             System.out.println("[" + tableCode + "] DONE. " + ssbApiCall.getMetadata().getTitle());
-            /*try {
-                System.out.println("[" + tableCode + "] STARTING. " + ssbApiCall.getMetadata().getTitle() + ". Size: " + ssbApiCall.getQuerySize());
-                monitor.increaseQueryCounter(querySize);
-                dfs.createTable(json);
-                dfs.insertData(json);
-                //ssbApiCall.tableApiCall();
-                System.out.println("[" + tableCode + "] DONE. " + ssbApiCall.getMetadata().getTitle());
-            } catch (IOException e) {
-                System.err.println(ssbApiCall.getMetadata().getTitle());
-                e.printStackTrace();
-            }
-
-             */
         }
     }
 }
