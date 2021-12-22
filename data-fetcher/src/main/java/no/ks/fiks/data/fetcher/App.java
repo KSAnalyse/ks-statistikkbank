abcbb3d3ad5acea045327133a1008cc7ade7a719
hamakeliKS <67731900+hamakeliKS@users.noreply.github.com>
2021-12-22 11:26:11 +0100

    Fikset opp noen pom dependency

--- data-fetcher/src/main/java/no/ks/fiks/data/fetcher/App.java
+++ data-fetcher/src/main/java/no/ks/fiks/data/fetcher/App.java
@@ -3,7 +3,6 @@ package no.ks.fiks.data.fetcher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class App {
     public static void main( String[] args ) {
